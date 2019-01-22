1d13ee61f5b0b3553ac9f6a306d71e62951c4d23
Arie Brainware <publicu001@gmail.com>
2019-01-22 23:27:52 +0700

    Add bot telegram library and add basic command

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const TelegramBot = require('node-telegram-bot-api')
+const qs = require('qs')
 const indexRouter = require('./routes/index');
 
 
@@ -33,4 +34,48 @@ app.use(function(err, req, res, next) {
   res.send('error');
 });
 
+
+// Bot usage
+
+require('dotenv-extended').load({
+  encoding: 'utf8',
+  silent: true,
+  path: '.env',
+  defaults: '.env.defaults',
+  schema: '.env.schema',
+  errorOnMissing: false,
+  errorOnExtra: false,
+  includeProcessEnv: false,
+  assignToProcessEnv: true,
+  overrideProcessEnv: false
+});
+
+const token = process.env.BOTTOKEN || '656586151:AAHx_RdJZB5kw3DdIUS01EJzPY59lCIyoJc'
+const bot = new TelegramBot(token, { polling: true })
+
+bot.onText(/\/menu/, (msg) => {
+  const response = {
+    reply_to_message_id: msg.message_id,
+    reply_markup: JSON.stringify({
+      keyboard: [
+        ['Nasi + Kangkung Tumis + Perkedel + Jeruk'],
+        ['Nasi + Daun Ubi Santan + Bakwan + Ayam Penyet']
+      ]
+    })
+    
+  }
+  bot.sendMessage(msg.chat.id, 'What do you want to order?', response)
+  return response.reply_markup.keyboard = []
+})
+
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id
+
+  const syntax = [
+    '/order',
+    '/menu',
+    '/checkPayment',
+  ]
+  bot.sendMessage(chatId, `Use this for order: ${syntax[0]} , this one for list menu ${syntax[1]} , and this one for check your payment status ${syntax[2]}`)
+})
 module.exports = app;
