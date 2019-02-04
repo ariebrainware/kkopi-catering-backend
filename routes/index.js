073f1b07f009c21b1182acbab65a3b5aad145ee4
Arie Brainware <publicu001@gmail.com>
2019-02-05 04:46:32 +0700

    Fix linting

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send({ 
     title: 'Kampung Kopi Backend',
     version:'v1.0'
-  });
-});
+  })
+})
 
-module.exports = router;
+module.exports = router
