eb280c41270b128ca161ac946b191760c891890c
Arie Brainware <publicu001@gmail.com>
2019-02-05 18:36:31 +0700

    Update and refine eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,30 @@
 module.exports = {
-    "env": {
-        "commonjs": true,
-        "es6": true,
-        "node": true
+    'env': {
+        'commonjs': true,
+        'es6': true,
+        'node': true
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    extends: ['eslint:recommended'],
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
     },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ],
-        "keyword-spacing": {
-            "before": true,
-            "after": true
-        },
-        "comma-dangle": [
-            "error",
-            "always",
-            "only-multiline"
-        ]
+    'rules': {
+        'indent': ['error', 2],
+        'linebreak-style': ['error', 'unix'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'never'],
+        'keyword-spacing': ['error', { 'before': true, 'after': true }],
+        'comma-dangle': ['error', 'always-multiline'],
+        'comma-spacing': ['error', { 'after': true, 'before': false }],
+        'no-trailing-spaces': ['error'],
+        'no-param-reassign': ['error', { props: false }],
+        'object-curly-newline': [0],
+        'lines-between-class-members': ['error', 'always'],
+        'arrow-body-style': 0,
+        'no-underscore-dangle': 0,
+        'no-nested-ternary': 1,
+        'no-duplicate-imports': 'error',
+        'no-multi-spaces': 'error'
     }
 };
