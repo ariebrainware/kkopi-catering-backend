84ace859bb98430c1913e763bcdeb1a3ff396aec
Arie Brainware <publicu001@gmail.com>
2019-02-09 16:30:14 +0700

    Refactor code

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.send({
     title: 'Kampung Kopi Backend',
     version: 'v1.0',
